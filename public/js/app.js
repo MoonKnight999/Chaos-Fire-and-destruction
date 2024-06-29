3c520ce2901de100e8ae12bb81b03388bde2d4eb
Moondip Konwar <moondipkonwar9@gmail.com>
2024-06-29 20:57:23 +0530

    Added PlayerB

--- public/js/app.js
+++ public/js/app.js
@@ -26,3 +26,32 @@ const PlayerA = new gameObject({
 },{
     speed: 3
 })
+
+const PlayerB = new gameObject({
+    id: "PlayerB",
+    groups: ["entity", "player"],
+    imageSrc: imagePaths["human"],
+    position: {x: 10, y: 10},
+    collider: new rectangularCollider({}, 16, 16)
+}, 
+()=>{// Player B update method
+    //Collider
+    PlayerB.collider.position = PlayerB.position
+
+    //Movement
+    if (Input.isKeyDown('ArrowUp')) {
+        PlayerB.position.y -= PlayerB.speed
+    }
+    else if (Input.isKeyDown('ArrowDown')) {
+        PlayerB.position.y += PlayerB.speed
+    }
+    else if (Input.isKeyDown('ArrowLeft')) {
+        PlayerB.position.x -= PlayerB.speed
+    }
+    else if (Input.isKeyDown('ArrowRight')) {
+        PlayerB.position.x += PlayerB.speed
+    }
+    
+},{
+    speed: 3
+})
