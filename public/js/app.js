f34ad89ba1b79b63d04c81ff3b51d7e20c1fba31
Moondip Konwar <moondipkonwar9@gmail.com>
2024-06-30 17:45:18 +0530

    Added PlayerA summon fire line

--- public/js/app.js
+++ public/js/app.js
@@ -2,41 +2,55 @@ const PlayerA = new gameObject({
     id: "PlayerA",
     groups: ["entity", "player"],
     imageSrc: imagePaths["human"],
-    position: {x: 10, y: 10},
+    position: { x: 10, y: 10 },
     collider: new rectangularCollider({}, 16, 16)
-}, 
-()=>{// Player A update method
-    //Collider
-    PlayerA.collider.position = PlayerA.position
+},
+    () => {// Player A update method
+        //Collider
+        PlayerA.collider.position = PlayerA.position
 
-    //Movement
-    if (Input.isKeyDown('w')) {
-        PlayerA.position.y -= PlayerA.speed
-    }
-    else if (Input.isKeyDown('s')) {
-        PlayerA.position.y += PlayerA.speed
-    }
-    else if (Input.isKeyDown('a')) {
-        PlayerA.position.x -= PlayerA.speed
-    }
-    else if (Input.isKeyDown('d')) {
-        PlayerA.position.x += PlayerA.speed
-    }
+        //Movement
+        if (Input.isKeyDown('w')) {
+            PlayerA.position.y -= PlayerA.speed
+        }
+        else if (Input.isKeyDown('s')) {
+            PlayerA.position.y += PlayerA.speed
+        }
+        else if (Input.isKeyDown('a')) {
+            PlayerA.position.x -= PlayerA.speed
+        }
+        else if (Input.isKeyDown('d')) {
+            PlayerA.position.x += PlayerA.speed
+        }
 
-    //Spawn Trees
-    if (Input.isKeyDown('t') && PlayerA.canSpawnTree) {
-        PlayerA.canSpawnTree = false
-        summonSapling({x: PlayerA.position.x, y: PlayerA.position.y + 16})
-        setTimeout(() => {
-            PlayerA.canSpawnTree = true
-        }, 1000);
-    }
-    
-},{
+        //Spawn Trees
+        if (Input.isKeyDown('t') && PlayerA.canSpawnTree) {
+            PlayerA.canSpawnTree = false
+            summonSapling({ x: PlayerA.position.x, y: PlayerA.position.y + 16 })
+            setTimeout(() => {
+                PlayerA.canSpawnTree = true
+            }, 1000);
+        }
+
+        //Spawn Fire
+        if (Input.isKeyDown('f') && PlayerA.canSpawnFire) {
+            PlayerA.canSpawnFire = false
+
+            //Summon fire line
+            for (let i = 1; i < 5; i++) {
+                summonFire({ x: (PlayerA.position.x - (i*16)), y: PlayerA.position.y + 16 })
+            }
+
+            setTimeout(() => {
+                PlayerA.canSpawnFire = true
+            }, 1000);
+        }
+    }, {
     speed: 3,
     canSpawnTree: true,
-    renderMethod: () =>{
-        game.ctx.fillText("Player A", PlayerA.position.x-8, PlayerA.position.y - 8)
+    canSpawnFire: true,
+    renderMethod: () => {
+        game.ctx.fillText("Player A", PlayerA.position.x - 8, PlayerA.position.y - 8)
     }
 })
 
@@ -44,40 +58,40 @@ const PlayerB = new gameObject({
     id: "PlayerB",
     groups: ["entity", "player"],
     imageSrc: imagePaths["human"],
-    position: {x: 10, y: 10},
+    position: { x: 10, y: 10 },
     collider: new rectangularCollider({}, 16, 16)
-}, 
-()=>{// Player B update method
-    //Collider
-    PlayerB.collider.position = PlayerB.position
+},
+    () => {// Player B update method
+        //Collider
+        PlayerB.collider.position = PlayerB.position
 
-    //Movement
-    if (Input.isKeyDown('ArrowUp')) {
-        PlayerB.position.y -= PlayerB.speed
-    }
-    else if (Input.isKeyDown('ArrowDown')) {
-        PlayerB.position.y += PlayerB.speed
-    }
-    else if (Input.isKeyDown('ArrowLeft')) {
-        PlayerB.position.x -= PlayerB.speed
-    }
-    else if (Input.isKeyDown('ArrowRight')) {
-        PlayerB.position.x += PlayerB.speed
-    }
+        //Movement
+        if (Input.isKeyDown('ArrowUp')) {
+            PlayerB.position.y -= PlayerB.speed
+        }
+        else if (Input.isKeyDown('ArrowDown')) {
+            PlayerB.position.y += PlayerB.speed
+        }
+        else if (Input.isKeyDown('ArrowLeft')) {
+            PlayerB.position.x -= PlayerB.speed
+        }
+        else if (Input.isKeyDown('ArrowRight')) {
+            PlayerB.position.x += PlayerB.speed
+        }
 
-    //Spawn Trees
-    if (Input.isMouseDown(Game.gameData.canvas).pressed && PlayerB.canSpawnTree) {
-        PlayerB.canSpawnTree = false
-        summonSapling(Input.isMouseDown(Game.gameData.canvas).position)
-        setTimeout(() => {
-            PlayerB.canSpawnTree = true
-        }, 1000);
-    }
-    
-},{
+        //Spawn Trees
+        if (Input.isMouseDown(Game.gameData.canvas).pressed && PlayerB.canSpawnTree) {
+            PlayerB.canSpawnTree = false
+            summonSapling(Input.isMouseDown(Game.gameData.canvas).position)
+            setTimeout(() => {
+                PlayerB.canSpawnTree = true
+            }, 1000);
+        }
+
+    }, {
     speed: 3,
     canSpawnTree: true,
-    renderMethod: () =>{
-        game.ctx.fillText("Player B", PlayerB.position.x-8, PlayerB.position.y - 8)
+    renderMethod: () => {
+        game.ctx.fillText("Player B", PlayerB.position.x - 8, PlayerB.position.y - 8)
     }
 })
